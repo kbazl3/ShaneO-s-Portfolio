977b27aac69b72151fa14a1e63024e1e6c1e601d
Kyle Barlow <kbazlo@yahoo.com>
2016-03-02 21:58:43 -0700

    added pagination directive with different views

--- js/app.js
+++ js/app.js
@@ -20,9 +20,34 @@ angular.module("portfolio", ["ui.router"])
                 templateUrl:"views/contact.html"
             })
 
-            .state("tattoos", {
-                url:"/tattoos",
-                templateUrl:"views/tattoos.html",
+            .state("tattoos1", {
+                url:"/tattoos/page1",
+                templateUrl:"views/tattoos/page1.html",
                 controller: "tattoosCtrl"
             })
+
+            .state("tattoos2", {
+                url:"/tattoos/page2",
+                templateUrl:"views/tattoos/page2.html",
+                controller: "tattoosCtrl"
+            })
+
+            .state("tattoos3", {
+                url:"/tattoos/page3",
+                templateUrl:"views/tattoos/page3.html",
+                controller: "tattoosCtrl"
+            })
+
+            .state("tattoos4", {
+                url:"/tattoos/page4",
+                templateUrl:"views/tattoos/page4.html",
+                controller: "tattoosCtrl"
+            })
+
+            .state("tattoos5", {
+                url:"/tattoos/page5",
+                templateUrl:"views/tattoos/page5.html",
+                controller: "tattoosCtrl"
+            })
+
     })
---
+++ js/directives/pagination.js
@@ -0,0 +1,17 @@
+angular.module("portfolio")
+    .directive("pagination", function() {
+        return {
+            templateUrl: "templates/pagination.html",
+            restrict: "E",
+            // link: function(scope, element, attributes){
+            //     element.on("click", function() {
+            //         addClass('active');
+            //     })
+            //
+            //
+            // }
+
+        }
+
+
+    })
